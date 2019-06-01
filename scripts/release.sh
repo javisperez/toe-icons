7c42f0bddef7eb721dfc87e2a60e197410c4da53
Javis <javisperez@gmail.com>
2019-05-31 22:02:17 -0400

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -21,48 +21,49 @@ if [ $VERSION = $PACKAGE_VERSION ]; then
   exit 1
 fi
 
-read -p "Releasing $VERSION - are you sure? (y/N) " -n 1 -r
+read -p "Releasing $VERSION - are you sure? (y/N) " continue
+
+if [[ $continue =~ ^[Nn]$ ]]; then
+  echo "Cancelled"
+  exit 1
+fi
 
 echo
+echo "* * * * * * * Releasing $VERSION * * * * * * *"
+echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo
-  echo "* * * * * * * Releasing $VERSION * * * * * * *"
-  echo
+# update package.json version to be used in the build
+npm version $VERSION --no-git-tag-version
 
-  # update package.json version to be used in the build
-  npm version $VERSION --no-git-tag-version
+# build
+echo "Building..."
+npm run build
 
-  # build
-  echo "Building..."
-  npm run build
+# generate the git tag
+echo "Tagging..."
+git tag v$VERSION
 
-  # generate the git tag
-  echo "Tagging..."
-  git tag v$VERSION
+# commit
+git add -A
+git commit -m "[build] $VERSION"
 
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
+# publish
+echo "Pushing..."
+git push origin refs/tags/v$VERSION
+git push
 
-  # publish
-  echo "Pushing..."
-  git push origin refs/tags/v$VERSION
-  git push
+echo "Publishing to NPM..."
+sed 's#"main": "./dist/components.js",#"main": "./components.js",#' package.json > ./dist/package.json
 
-  echo "Publishing to NPM..."
-  sed 's#"main": "./dist/components.js",#"main": "./components.js",#' package.json > ./dist/package.json
+cd ./dist
+npm publish
 
-  cd ./dist
-  npm publish
+# update the docs
+echo "Updating the docs..."
+cd ..
+npm run docs
 
-  # update the docs
-  echo "Updating the docs..."
-  cd ..
-  npm run docs
-
-  echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-  echo "* All done!, now remember to add the release information on the github repo:  *"
-  echo "*           https://github.com/javisperez/toe-icons/releases                  *"
-  echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-fi
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo "* All done!, now remember to add the release information on the github repo:  *"
+echo "*           https://github.com/javisperez/toe-icons/releases                  *"
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
