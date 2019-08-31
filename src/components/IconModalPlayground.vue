022b80c4e26d9e4f0dcbcbb58da0bd027e6e0c6b
Javis <javisperez@gmail.com>
2019-08-31 11:38:30 -0400

    Temporary disabled the secondary icon combination due css build error

--- src/components/IconModalPlayground.vue
+++ src/components/IconModalPlayground.vue
@@ -2,7 +2,7 @@
 import Vue from "vue";
 import { Chrome as ColorPicker } from "vue-color";
 import ClickOutside from "vue-click-outside";
-import Multiselect from "vue-multiselect";
+// import Multiselect from "vue-multiselect";
 import iconsJson from "../../dist/icons.json";
 import { capitalize } from "lodash";
 
@@ -11,7 +11,7 @@ export default Vue.extend({
 
   components: {
     ColorPicker,
-    Multiselect
+    // Multiselect
   },
 
   directives: {
@@ -203,7 +203,7 @@ export default Vue.extend({
     <div
       class="flex flex-col justify-center items-center flex-auto bg-white p-4"
     >
-      <div class="w-full z-10">
+      <!-- <div class="w-full z-10">
         <div class="flex justify-start w-full items-center">
           <label>
             <input type="checkbox" v-model="isSecondaryIconEnabled" />
@@ -273,7 +273,7 @@ export default Vue.extend({
             >
           </div>
         </div>
-      </div>
+      </div> -->
 
       <div class="flex-1 w-full text-center justify-center items-center flex">
         <span class="relative block">
@@ -299,123 +299,6 @@ export default Vue.extend({
         </span>
       </div>
     </div>
-    <!-- <div
-      class="flex flex-col border-0 border-r border-solid border-grey-lighter pr-4"
-    >
-      <div class="text-sm rounded p-1 cursor-pointer mb-2">
-        <div class="font-bold">Icon Fill Color</div>
-        <div
-          class="inline-flex items-center"
-          v-click-outside="
-            () => {
-              hidePicker('fill');
-            }
-          "
-        >
-          <div
-            class="flex items-center inline-block p-2 hover:bg-white rounded"
-            @click="isFillColorPickerOpen = !isFillColorPickerOpen"
-          >
-            <span
-              :style="`background-color: ${fillColor}`"
-              class="w-4 h-4 rounded mr-2"
-            />
-            {{ fillColor }}
-          </div>
-
-          <div class="absolute" v-show="isFillColorPickerOpen">
-            <ColorPicker
-              :value="fillColor"
-              @input="
-                ({ hex }) => {
-                  fillColor = hex;
-                }
-              "
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="text-sm rounded p-1 cursor-pointer mb-2">
-        <div class="font-bold">
-          Icon Size:
-          <span class="ml-1 text-cyan">{{ size }}px</span>
-        </div>
-        <input type="range" min="8" max="128" v-model="size" step="1" />
-      </div>
-
-      <div class="text-sm rounded p-1 cursor-pointer mb-2">
-        <div
-          class="font-bold flex items-center"
-          @click="isRotating = !isRotating"
-        >
-          Rotate
-          <ti
-            :icon="isRotating ? 'toggle-on' : 'toggle-off'"
-            size="28"
-            class="text-black"
-          />
-        </div>
-      </div>
-
-      <div class="text-sm rounded p-1 cursor-pointer mb-2">
-        <div
-          class="font-bold flex items-center"
-          @click="isStroked = !isStroked"
-        >
-          Stroked
-          <ti
-            :icon="isStroked ? 'toggle-on' : 'toggle-off'"
-            size="28"
-            class="text-black"
-          />
-        </div>
-      </div>
-
-      <div class="text-sm rounded p-1 cursor-pointer mb-2" v-if="isStroked">
-        <div class="font-bold flex items-center">
-          Stroke Size:
-          <span class="ml-1 text-cyan">{{ strokeSize }}</span>
-        </div>
-        <input type="range" min="0" max="4" v-model="strokeSize" step="1" />
-      </div>
-
-      <div class="text-sm rounded p-1 cursor-pointer mb-2" v-if="isStroked">
-        <div class="font-bold">Icon Stroke Color</div>
-        <div
-          class="inline-flex items-center"
-          v-click-outside="
-            () => {
-              hidePicker('stroke');
-            }
-          "
-        >
-          <div
-            class="flex items-center inline-block p-2 hover:bg-white rounded"
-            @click="isStrokeColorPickerOpen = !isStrokeColorPickerOpen"
-          >
-            <span
-              :style="`background-color: ${strokeColor}`"
-              class="w-4 h-4 rounded mr-2"
-            />
-            {{ strokeColor }}
-          </div>
-
-          <div class="absolute" v-show="isStrokeColorPickerOpen">
-            <ColorPicker
-              :value="strokeColor"
-              @input="
-                ({ hex }) => {
-                  strokeColor = hex;
-                }
-              "
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    -->
   </div>
 </template>
 
