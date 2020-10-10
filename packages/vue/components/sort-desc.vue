4f03cdbfa79986d47dc5a8ae51a7440dc7f6497b
Javis <javis.perez@packetfabric.com>
2020-10-10 16:57:15 -0400

    update sort icons alpha

--- packages/vue/components/sort-desc.vue
+++ packages/vue/components/sort-desc.vue
@@ -16,6 +16,6 @@ export default {
     :width="size" :height="size"
     viewBox="0 0 64 64" :fill="color" :stroke="strokeColor"
     :stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M31.966 53.494L15.232 36.759l2.1-2.1L31.975 49.3 46.68 34.597l2.091 2.092-16.805 16.805z" fill-rule="nonzero"></path><path d="M32.034 10.506l16.734 16.735-2.1 2.1L32.025 14.7 17.32 29.403l-2.091-2.092 16.805-16.805z" style="opacity: 0.45" fill-rule="nonzero"></path>
+    <path d="M31.966 53.494L15.232 36.759l2.1-2.1L31.975 49.3 46.68 34.597l2.091 2.092-16.805 16.805z" fill-rule="nonzero"></path><path d="M32.034 10.506l16.734 16.735-2.1 2.1L32.025 14.7 17.32 29.403l-2.091-2.092 16.805-16.805z" style="opacity: 0.15" fill-rule="nonzero"></path>
   </svg>
 </template>
